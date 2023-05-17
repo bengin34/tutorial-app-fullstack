3e898ca3afaa1ef52331b70783b49c5405072e9b
bengin34 <becaglar3434@gmail.com>
2023-05-17 21:16:24 +0300

    models -serializers

--- api/tutorial/views.py
+++ api/tutorial/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Tutorial
+from .serializers import TutorialSerializer
+from rest_framework.viewsets import ModelViewSet
+
+
 
 # Create your views here.
+class TutorialView(ModelViewSet):
+    pass
